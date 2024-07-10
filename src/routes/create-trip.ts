d194c69d7b3b7440c26d15c05a52cf00cc28e076
James Meneses <james.meneseswk@gmail.com>
2024-07-09 22:19:03 -0300

    Sets participants relation data when creating a new trip

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -18,12 +18,19 @@ export async function createTrip(app: FastifyInstance) {
           ends_at: z.coerce.date(),
           owner_name: z.string(),
           owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
         }),
       },
     },
     async (request) => {
-      const { destination, starts_at, ends_at, owner_name, owner_email } =
-        request.body;
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_to_invite,
+      } = request.body;
 
       if (dayjs(starts_at).isBefore(new Date())) {
         throw new Error("Invalid start date");
@@ -42,11 +49,20 @@ export async function createTrip(app: FastifyInstance) {
           ends_at,
 
           participants: {
-            create: {
-              name: owner_name,
-              email: owner_email,
-              is_owner: true,
-              is_confirmed: true,
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return {
+                    email,
+                  };
+                }),
+              ],
             },
           },
         },
