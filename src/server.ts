2f5251ef59c8f9eddd601adf61a933f5aecf4502
James Meneses <james.meneseswk@gmail.com>
2024-07-13 19:37:49 -0300

    Sets the create activities and get activities list routes

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import {
 } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participants";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
 
 const app = fastify();
 
@@ -21,6 +23,8 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipants);
+app.register(createActivity);
+app.register(getActivities);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server running");
