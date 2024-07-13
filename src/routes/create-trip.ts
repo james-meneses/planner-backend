0ac99e5c900689b6c02965bdc86432d2534949a7
James Meneses <james.meneseswk@gmail.com>
2024-07-13 14:38:16 -0300

    Adds confirmtrip endpoint and sets the application cors

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -76,6 +76,8 @@ export async function createTrip(app: FastifyInstance) {
       const formattedStartDate = dayjs(starts_at).format("LL");
       const formattedEndDate = dayjs(ends_at).format("LL");
 
+      const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`;
+
       const mail = await getMailClient();
 
       const message = await mail.sendMail({
@@ -94,7 +96,7 @@ export async function createTrip(app: FastifyInstance) {
         <p></p>
         <p>Para confirmar sua viagem, clique no link abaixo:</p>
         <p>
-        <a href="">Confirmar Viagem</a>
+        <a href="${confirmationLink}">Confirmar Viagem</a>
         </p>
         <p>Se você não solicitou essa viagem, por favor, ignore este e-mail.</p>
       </div>
